cf996837ad5dd82a1b38d3b6863b16b4b519543a
AMoinian2004 <44126226+AMoinian2004@users.noreply.github.com>
2022-03-09 17:01:21 -0500

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"Bridgewater",
+        "defaultValue":"Mount Olive",
         "required":"true",
         "disabled":"true"
       },
